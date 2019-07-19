3a4025d368b2631c1d270d04c20c808c0553537f
lookwendyican <wendyalers@gmail.com>
2019-07-19 19:42:59 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDc7ibqPSFSZsxVmAyKe84tp00peFyJ3pc",
+    authDomain: "train-scheduler-d4c9e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d4c9e.firebaseio.com",
+    projectId: "train-scheduler-d4c9e",
+    storageBucket: "",
+    messagingSenderId: "180241548457",
+    appId: "1:180241548457:web:43d1b24d4d8833a3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  var database = firebase.database();
